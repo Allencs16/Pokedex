a9b14270f258ee1ae4bf4810b095207ee6eceac4
Breno Alencar <brenoallencs@gmail.com>
2021-12-30 23:47:51 -0300

    Estrutura header

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import {Header} from './styles';
+import {ButtonSearch, Header, Navbar} from './styles';
+import '../../App.css'
 
 function HomePage(){
   return (
-    <Header>
-      <h1>Pokédex</h1>
-    </Header>
+    <div className="body">
+      <Header>
+        <h3>Pokédex</h3>
+      </Header>
+      <Navbar>
+        <div className="titleNav">
+          <form>
+            <label>Name or Number <br />
+              <input type="text" name="name" />
+              <ButtonSearch />
+            </label>
+          </form>
+        </div>
+        <div className="empty" />
+      </Navbar>
+    </div>
   )
 }
 
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,20 +1,36 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  align-itens: center;
-  width: auto;
-  height: 100%;
-
-  background-color: #CECE;
-`;
-
 export const Header = styled.div`
   display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  width: 80%;
+  height: 3rem;
+
+  color: #cecece;
+`;
+
+export const Navbar = styled.div`
+  display: flex;
   justify-content: center;
-  align-itens: center;
+  align-items: center;
 
   width: 100%;
-  height: 3rem;
+  height: auto;
+
+  background-color: #222;
+  color: #fff;
+`;
+
+export const ButtonSearch = styled.button`
+  height: 2.2rem;
+  width: 2.2rem;
+
+  border: none;
+  border-radius: 3px;
+
+  margin-left: 1rem;
+
+  background-color: #ff6600;
 `;
